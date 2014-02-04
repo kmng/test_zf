0bcb3f66622b7927e7664aa445207a41e32b9c56
Server Template <kng@ubuntu.(none)>
2014-02-04 12:03:52 -0800

    Add all

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,20 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=zf;host=localhost',
+        'username'         => 'zf',
+        'password'         => 'zf',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+    
 );
