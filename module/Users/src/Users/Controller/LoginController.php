bc3604b9fdd1ca95f0630e3673c8aac3fbb1ef1a
kmng <kinmanng@usc.edu>
2014-02-04 14:51:46 -0800

    update login form

--- module/Users/src/Users/Controller/LoginController.php
+++ module/Users/src/Users/Controller/LoginController.php
@@ -4,9 +4,26 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\Authentication\AuthenticationService;
 use Zend\Authentication\Adapter\DbTable as DbTableAuthAdapter;
+use Users\Form\LoginForm;
+use Users\Form\LoginFilter;
+
+use Users\Model\User;
+use Users\Model\UserTable;
 
 class LoginController extends AbstractActionController
 {
+
+	protected $storage;
+    protected $authservice;
+
+    public function indexAction()
+    {
+		$form = new LoginForm();
+		$viewModel  = new ViewModel(array('form' => $form)); 
+		return $viewModel; 
+    }
+
+
 	public function getAuthService()
 	{
 	  if (! $this->authservice) {
@@ -20,20 +37,51 @@ class LoginController extends AbstractActionController
 	}
 
 	public function processAction()
-	{
-	
-		$this->getAuthService()->getAdapter()
-			->setIdentity($this->request->getPost('email'))
-			->setCredential($this->request->getPost('password'));
-		$result = $this->getAuthService()->authenticate();
-		if ($result->isValid()) {
-	  		$this->getAuthService()->getStorage()->write($this->request->getPost('email'));
-	  		return $this->redirect()->toRoute(NULL , array( 
-	                                      'controller' => 'login', 
-	                                      'action' =>  'confirm' 
-	                                      ));			
+    {
+        if (!$this->request->isPost()) {
+            return $this->redirect()->toRoute(NULL , array( 
+                        'controller' => 'login', 
+                        'action' =>  'index' 
+                    ));
+        }
+
+        $post = $this->request->getPost();
+
+        $form = new LoginForm();
+		$inputFilter = new LoginFilter();
+		$form->setInputFilter($inputFilter);
+
+        $form->setData($post);
+        if (!$form->isValid()) {
+            $model = new ViewModel(array(
+                'error' => true,
+                'form'  => $form,
+            ));
+            $model->setTemplate('users/login/index');
+            return $model;
+        } else {
+			//check authentication...
+			$this->getAuthService()->getAdapter()
+								   ->setIdentity($this->request->getPost('email'))
+								   ->setCredential($this->request->getPost('password'));
+            $result = $this->getAuthService()->authenticate();
+            if ($result->isValid()) {
+				$this->getAuthService()->getStorage()->write($this->request->getPost('email'));
+				return $this->redirect()->toRoute(NULL , array( 
+								'controller' => 'login', 
+								'action' =>  'confirm' 
+							));			
+            } else {
+				$model = new ViewModel(array(
+					'error' => true,
+					'form'  => $form,
+				));
+				$model->setTemplate('users/login/index');
+				return $model;
+			}
 		}
-	}	
+    }
+
 
 	public function confirmAction()
 	{
